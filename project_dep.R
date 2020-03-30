633f95c1182fe4d8803f52121ade275b86460c13
Sean Atpr <seanatpr@gmail.com>
2020-03-30 01:19:40 -0400

    project: clean up files

--- project_dep.R
+++ project_dep.R
@@ -2,11 +2,11 @@ source('project_init.R', local = TRUE)
 
 
 # Install -----------------------------------------------------------------
-install.packages("partykit")
-devtools::install_github("ibecav/CGPfunctions", build_vignettes = TRUE)
-install.packages("CHAID", repos="http://R-Forge.R-project.org")
+# install.packages("partykit")
+# devtools::install_github("ibecav/CGPfunctions", build_vignettes = TRUE)
+# install.packages("CHAID", repos="http://R-Forge.R-project.org")
 
 
 # Download data -----------------------------------------------------------
 df_working <- read.csv("https://raw.githubusercontent.com/quants-book/CSV_Files/master/churn.csv")
-write_csv(df_working, FILE_RAW_DATA)
+write_csv(x = df_working, path = FILE_DATA_RAW)
